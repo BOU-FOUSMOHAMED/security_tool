8a03eafb8a07a5d34c4041f72984560e13c9d309
BOU-FOUSMOHAMED <you@example.com>
2026-08-03 13:45:19 +0200

    afficher les resultats directly v1

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def scan():
     try:
         start = time.monotonic()
         proc = subprocess.run(
-            ["nmap","-Pn","-sT","-n", target],
+            ["nmap","-Pn","-sT","-n","oX","-", target],
             capture_output=True, text=True, timeout=SCAN_TIMEOUT,
         )
         # result = parse_scan(proc.stdout)
