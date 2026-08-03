1158ce948e51359ca1dbd8fdcbdd402db63aeeb9
BOU-FOUS MOHAMED <boufousmohamed2005@gmail.com>
2026-08-03 17:44:27 +0200

    Change default port from 5000 to 8080

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def scan():
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("HOST", "0.0.0.0"),
-        port=int(os.environ.get("PORT", 5000)),
+        port=int(os.environ.get("PORT", 8080)),
         threaded=False,
     )
 
